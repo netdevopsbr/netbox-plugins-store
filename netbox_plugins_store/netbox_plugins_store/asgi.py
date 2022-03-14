7682f0458db2b1d176c7b25474b565e4cad95f3e
Emerson Pereira <emersonfelipe.2003@gmail.com>
2022-03-13 23:07:17 -0300

    Initial Deploy of Django Project

---
+++ netbox_plugins_store/netbox_plugins_store/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for netbox_plugins_store project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.0/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'netbox_plugins_store.settings')
+
+application = get_asgi_application()
---
+++ netbox_plugins_store/netbox_plugins_store/views.py
@@ -0,0 +1,21 @@
+import platform
+import sys
+
+from django.conf import settings
+from django.shortcuts import redirect, render
+
+from django.template import loader
+from django.template.exceptions import TemplateDoesNotExist
+
+from django.urls import reverse
+
+from django.views.generic import View
+
+class HomeView(View):
+    """Homepage"""
+    template_name = 'netbox_plugins_store/home.html'
+
+    # service incoming GET HTTP requests
+    def get(self, request):
+        """Get request."""
+        return render(request, self.template_name,)
