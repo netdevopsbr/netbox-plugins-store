a022ec570d1ac6009ddb5537d69786e6ca5618c0
Emerson Pereira <emersonfelipe.2003@gmail.com>
2022-03-15 01:33:58 -0300

    Initial view rendering static github json repo's

--- netbox_plugins_store/netbox_plugins_store/settings.py
+++ netbox_plugins_store/netbox_plugins_store/settings.py
@@ -133,6 +133,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 STATIC_ROOT = BASE_DIR + '/static'
 STATIC_URL = 'static/'
+#STATICFILES_DIRS = [
+#    BASE_DIR + '/static',
+#]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
--- netbox_plugins_store/netbox_plugins_store/views.py
+++ netbox_plugins_store/netbox_plugins_store/views.py
@@ -11,6 +11,8 @@ from django.urls import reverse
 
 from django.views.generic import View
 
+from . import repositories
+
 class HomeView(View):
     """Homepage"""
     template_name = 'netbox_plugins_store/home.html'
@@ -18,4 +20,10 @@ class HomeView(View):
     # service incoming GET HTTP requests
     def get(self, request):
         """Get request."""
-        return render(request, self.template_name,)
+        return render(
+            request,
+            self.template_name,
+            {
+                "github": repositories.github_json,
+            },
+        )
