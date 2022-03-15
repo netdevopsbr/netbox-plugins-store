f29bab59b74e9d1dc82f8b53dc40fc8dbf6a4ff6
emersonfelipesp <emersonfelipe.2003@gmail.com>
2022-03-15 20:20:53 +0000

    Initial work to transform repo into plugin

---
+++ netbox_plugins_store/netbox_plugins_store/navigation.py
@@ -0,0 +1,9 @@
+from extras.plugins import PluginMenuButton, PluginMenuItem
+from utilities.choices import ButtonColorChoices
+
+menu_items = (
+    PluginMenuItem(
+        link='plugins:netbox_animal_sounds:random_animal',
+        link_text='Random sound',
+    ),
+)
