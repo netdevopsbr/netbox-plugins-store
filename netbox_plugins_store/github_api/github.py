730b460fac8d88b31ea88d83eb105b8f9bd94130
emersonfelipesp <emersonfelipe.2003@gmail.com>
2022-03-17 19:16:54 +0000

    add search_github_json(plugin_name) function and 'github_read_token' attr to GitHubAPI

--- netbox_plugins_store/github_api/github.py
+++ netbox_plugins_store/github_api/github.py
@@ -3,19 +3,36 @@ import re
 import json
 import math
 
-
 class GitHubAPI():
-    def get_netbox_repos(self):
+    def get_netbox_json(self, **kwargs):
+        '''
+            Get JSON from "https://github.com/emersonfelipesp/netbox-plugins-store/blob/develop/repositories.json"
+        '''
 
+        json_type = kwargs.get('json_type', 'summary')
+
+        # Define JSON to retrieve from GitHub emersonfelipesp/netbox-plugins-store
+        if json_type == 'all':
+            github_file_name = 'repositories.json'
+        elif json_type == 'summary':
+            github_file_name = 'repositories_github.json'
+        else:
+            github_file_name = '[ERROR] Not possible to define what JSON to retrieve from GitHub'
+
+
+        ### GitHub Data
         github_user = 'emersonfelipesp'
         github_repo_name = 'netbox-plugins-store'
-        github_file_name = 'repositories.json'
+        github_api_url = self.api_url
+        github_read_token = self.read_token
+
 
         # HTTP Request
         get_response = requests.get(
-            f'https://api.github.com/repos/{github_user}/{github_repo_name}/contents/{github_file_name}',
+            f'{github_api_url}/repos/{github_user}/{github_repo_name}/contents/{github_file_name}',
             headers = {
-                "Accept": "application/vnd.github.v3+json"
+                "Accept": "application/vnd.github.v3+json",
+                "Authorization": f"{github_read_token}"
             }
         ).json()
 
@@ -23,7 +40,10 @@ class GitHubAPI():
 
         # Get Raw Content of 'repositories.json' file from GitHub 'emersonfelipesp/netbox-plugins-store' repo
         get_json_file = requests.get(
-            raw_content_url
+            raw_content_url,
+            headers = {
+                "Authorization": f"{github_read_token}"
+            }
         )
 
         # Convert HTTP response to 'dict'
@@ -31,14 +51,22 @@ class GitHubAPI():
         return repositories
 
 
+    def get_repos_name(self):
+        '''
+            Look for Netbox Repositories names going to:
+            "github.com/topics/netbox-plugin"
+        '''
 
-
-    def create_netbox_repos(self):
         github_url = self.url
-        github_api_url = self.api_url
+        github_read_token = self.read_token
 
         # Get Github topics/netbox-plugin page
-        page = requests.get(f'{github_url}/topics/netbox-plugin')
+        page = requests.get(
+            f'{github_url}/topics/netbox-plugin',
+            headers = {
+                "Authorization": f"{github_read_token}"
+            }
+        )
         text = page.text
 
         # Look through page content for the number of public repositories
@@ -51,7 +79,7 @@ class GitHubAPI():
             number_of_pages = 1
 
         # Create empty array to received repositories found on page
-        repos = []
+        repos_name = []
 
         # Loop until fetch all pages needed to match the number of repos (30 repos per page)
         i = 1
@@ -60,7 +88,12 @@ class GitHubAPI():
             # If not first page (results are already acccessable from the first request), fetch i'th page
             if not i == 1:
                 page = requests.get(
-                    f'https://github.com/topics/netbox-plugin?page={i}')
+                    f'{github_url}/topics/netbox-plugin?page={i}',
+                    headers = {
+                        "Authorization": f"{github_read_token}"
+                    }
+                )
+                    
                 text = page.text
 
             # Search for this regex match on page results
@@ -70,26 +103,60 @@ class GitHubAPI():
             # Loop through every result, separating regular text from repo /author/name
             for item in regex_match:
                 item = re.findall(r'\/[\w,\d\-]*\/[\w,\d\-]*', item)
-                repos.append(item[0])
+                repos_name.append(item[0])
 
             i += 1
+        
+        return repos_name
+    
+
+    def get_repo_json(self, repository_name):
+        '''Based on repository name, returns JSON from GitHub API'''
+
+        github_api_url = self.api_url
+        github_read_token = self.read_token
+
+        # HTTP Request
+        data = requests.get(
+            f'{github_api_url}/repos{repository_name}',
+            headers = {
+                "Accept": "application/vnd.github.v3+json",
+                "Authorization": f"{self.github_read_token}"
+            }
+        )
+
+        # Convert HTTP Response to JSON
+        data = data.json()
+
+        # Check for TOKEN error
+        if data.get('message') == 'Bad credentials':
+            print("Token is wrong.")
+            return
+
+        # Return GitHub API Repository JSON
+        return data
+
+
+    def get_repos_all(self):
+        '''Saves GitHub API (Repos Endpoint) to list without treating JSON'''
+        repos = self.get_repos_name()
 
         repositories = []
+
         for repo in repos:
-            # HTTP Request
-            data = requests.get(
-                f'http://api.github.com/repos{repo}',
-                headers={
-                    "Authorization": f"token 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"
-                }
-            )
+            data = self.get_repo_json(repo)
+            repositories.append(info)
 
-            # Convert HTTP Response to JSON
-            data = data.json()
+        return repositories
 
-            if data.get('message') == 'Bad credentials':
-                print("Token is wrong.")
-                return
+
+    def get_repos_summary(self):
+        repos = self.get_repos_name()
+
+        repositories = []
+
+        for repo in repos:
+            data = self.get_repo_json(repo)
 
             info = {
                 "id": data["id"],
@@ -120,19 +187,26 @@ class GitHubAPI():
 
         return repositories
 
-    def __init__(self, url, api_url):
+
+    def __init__(self, github_url, github_api_url, github_read_token):
 
         # GitHub Parameters to run API
-        self.url = url
-        self.api_url = api_url
+        self.url = github_url
+        self.api_url = github_api_url
+        self.read_token = github_read_token
 
 
-def write_json_file(repos_json):
+def write_json_file(repos_json, **file_params):
+    '''
+        Write JSON passed to file using FILENAME provided or default "repositories.json"
+    '''
+    filename = str(file_params.get('filename', 'repositories_fixed.json'))
+
     # Open file
-    f = open('repositories_fixed.json', 'w')
+    f = open(f'{filename}', 'w')
 
     # Write returned JSON to file
-    f.write(repos_json)
+    f.write(json.dumps(repos_json))
 
 
 def check_for_json_file():
@@ -164,7 +238,29 @@ def check_for_json_file():
 
 
 # JSON returned from GitHub
-repositories = GitHubAPI(
+github_object = GitHubAPI(
     'https://github.com',
     'http://api.github.com',
-).get_netbox_repos()
+    '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',
+)
+
+# Useful functions
+repos_json_all = github_object.get_netbox_json(json_type = 'all')
+repos_json_summary = github_object.get_netbox_json(json_type = 'summary')
+repos_name = github_object.get_repos_name()
+
+
+def search_github_json(plugin_name):
+    # Check if NAME provided in URL is Plugin
+    for repo_name in repos_name:
+        # If Plugin name found, look for JSON
+        if plugin_name in repo_name:
+            for json in repos_json_all:
+                # If GitHub JSON of Plugin found, return it.
+                json_r = json.get('name')
+                if json_r == plugin_name:
+                    return json
+
+                
+        
+        
