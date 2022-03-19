1512437286019377528ff35bc3eebd0498ee83d1
emersonfelipesp <emersonfelipe.2003@gmail.com>
2022-03-19 17:24:41 +0000

    add Slack and GitHub Discussions link

--- netbox_plugins_store/navigation.py
+++ netbox_plugins_store/navigation.py
@@ -10,8 +10,16 @@ menu_items = (
         link='plugins:netbox_plugins_store:plugin_development',
         link_text='Plugins Development',
         buttons=(
-            PluginMenuButton('plugins:netbox_plugins_store:readthedocs', 'Plugins Development - ReadTheDocs', 'mdi mdi-file-document-multiple', ButtonColorChoices.BLUE),
+            PluginMenuButton('plugins:netbox_plugins_store:readthedocs', 'Plugins Development - ReadTheDocs', 'mdi mdi-slack', ButtonColorChoices.RED),
             PluginMenuButton('plugins:netbox_plugins_store:github', 'Plugins Development - GitHub', 'mdi mdi-github', ButtonColorChoices.BLACK),
+        )
+    ),
+    PluginMenuItem(
+        link='plugins:netbox_plugins_store:community',
+        link_text='Netbox Community',
+        buttons=(
+            PluginMenuButton('plugins:netbox_plugins_store:slack', 'Official Slack Community', 'mdi mdi-file-document-multiple', ButtonColorChoices.BLUE),
+            PluginMenuButton('plugins:netbox_plugins_store:github-discussion', 'GItHub Discussions', 'mdi mdi-github', ButtonColorChoices.BLACK),
         )
     ),
 )
